66dbe1d05feb0b2006d7eab1faa73e7c979a324b
Preston Parsons <ogaugeandcode@gmail.com>
2024-03-29 21:41:43 -0400

    Update cliveros.cpp to v0.102
    // AUTHORS: Preston Parsons, Noah Russeau, Ava Curby
    // GITHUB: TheGamingScout2, NoahR1024, transorsmth
    // CLIVER OS 
    //A Personal Project for the Prof
    // ALPHA v0.102 Testing Environment
    // *** PATCH NOTES ***
    //
    // v0.103-Alpha  - In Development
    // v0.102-Alpha - converted original hash map command data structure into
    //                linked list node-pointer style to increase speed and to allow
    //		  for the new command loading architecture style.

--- cliveros.cpp
+++ cliveros.cpp
@@ -4,13 +4,25 @@
 #include <map>
 #include <cstdlib>
 
-
 // AUTHORS: Preston Parsons, Noah Russeau, Ava Curby
 // GITHUB: TheGamingScout2, NoahR1024, transorsmth
 // CLIVER OS 
 //A Personal Project for the Prof
-
+// ALPHA v0.102 Testing Environment
+// *** PATCH NOTES ***
+//
+// v0.103-Alpha  - In Development
+// v0.102-Alpha - converted original hash map command data structure into
+//                linked list node-pointer style to increase speed and to allow
+//		  for the new command loading architecture style.  
 using namespace std;
+
+// Struct to represent a command
+struct Command {
+    string name;
+    void (*function)();
+    Command* next;
+};
 
 // Function prototypes
 void help();
@@ -19,22 +31,30 @@ void cliver();
 void create();
 void edit();
 void save();
-//void openFile(const string& filename);
 void quit();
 
-// Map to associate command strings with their corresponding functions
-map<string, void(*)()> commandMap = {
-    {"help", help},
-    {"print", print},
-    {"cliver", cliver},
-    {"create", create},
-    {"edit", edit},
-    {"save", save},
-    {"quit", quit}
-//    {"open", openFile}
-};
+// Linked list to store commands
+Command* commandList = nullptr;
+
+// Function to add a command to the linked list
+void addCommand(const string& name, void (*function)()) {
+    Command* newCommand = new Command;
+    newCommand->name = name;
+    newCommand->function = function;
+    newCommand->next = commandList;
+    commandList = newCommand;
+}
 
 int main() {
+    // Add commands to the linked list
+    addCommand("help", help);
+    addCommand("print", print);
+    addCommand("cliver", cliver);
+    addCommand("create", create);
+    addCommand("edit", edit);
+    addCommand("save", save);
+    addCommand("quit", quit);
+
     string command;
 
     cout << "Welcome to CLIVER OS v0.1\n";
@@ -44,14 +64,18 @@ int main() {
         cout << "> ";
         getline(cin, command);
 
-        // Parse the command
-        size_t spacePos = command.find(' ');
-        string cmd = (spacePos == string::npos) ? command : command.substr(0, spacePos);
+        // Traverse the linked list to find the command
+        Command* currentCommand = commandList;
+        while (currentCommand != nullptr) {
+            if (currentCommand->name == command) {
+                currentCommand->function();
+                break;
+            }
+            currentCommand = currentCommand->next;
+        }
 
-        // Execute the command
-        if (commandMap.find(cmd) != commandMap.end()) {
-            commandMap[cmd]();
-        } else {
+        // If command not found
+        if (currentCommand == nullptr) {
             cout << "Command not found. Type 'help' for available commands.\n";
         }
     }
@@ -68,16 +92,15 @@ void help() {
     cout << "  create - Create a new text file\n";
     cout << "  edit   - Edit an existing text file\n";
     cout << "  save   - Save changes to a text file\n";
-    cout << "  open   - Open a Text File\n";
     cout << "  quit   - Exit the operating system\n";
-    cout << "Written by Preston Parsons 2024";
+    cout << "Written by Preston Parsons, Noah Rousseau 2024, ";
     cout << "Version 0.1 - Development Alpha";
 }
 
 void print() {
-	cout << "Enter Text to Print: ";
-	string text;
-	getline(cin, text);
+    cout << "Enter Text to Print: ";
+    string text;
+    getline(cin, text);
     cout << text << endl;
 }
 
@@ -88,17 +111,15 @@ void cliver() {
     ifstream fin;
     fin.open("./secret");
 
-    if(!fin)
-    {
-        cout<<"Someone had an oopsie, cry!"<<endl;
+    if (!fin) {
+        cout << "Someone had an oopsie, cry!" << endl;
         return;
     }
 
-    while(!fin.eof())
-    {
+    while (!fin.eof()) {
         string sline = "";
         getline(fin, sline);
-        cout<<sline<<endl;
+        cout << sline << endl;
     }
 }
 
@@ -132,8 +153,8 @@ void edit() {
             while (getline(cin, line)) {
                 // Check for end of input or Ctrl+Z (EOF)
                 if (cin.eof()) {
-                	outFile.close();
-                	cout << "Changes saved to '" << filename << "'.\n";
+                    outFile.close();
+                    cout << "Changes saved to '" << filename << "'.\n";
                     return;
                 }
                 outFile << line << endl;
@@ -163,20 +184,6 @@ void save() {
     } else {
         cout << "Unable to save changes to text file.\n";
         return; // this is necessary to prevent a very long loop
-    }
-}
-
-void openFile(const string& filename) {
-    ifstream file(filename);
-    if (file.is_open()) {
-        string line;
-        cout << "Contents of '" << filename << "':\n";
-        while (getline(file, line)) {
-            cout << line << endl;
-        }
-        file.close();
-    } else {
-        cout << "Unable to open file '" << filename << "'.\n";
     }
 }
 
