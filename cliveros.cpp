65e7b8fd9c525b58dee2e6ed05eb78b629ce1a9b
Preston Parsons <ogaugeandcode@gmail.com>
2024-03-30 00:33:53 -0400

    Update cliveros.cpp
    // ALPHA v0.103-Alpha - In Development
    // *** PATCH NOTES ***
    // - Converted original hash map command data structure into
    //   linked list node-pointer style to increase speed and to allow
    //   for the new command loading architecture style.  
    // - Added commands HELLO, NEW, OLD, REPLACE, RENAME, CAT, LIST, RUN, STOP,
    //   UNSAVE, SYSTEM, BYE, GOODBYE as per specifications.

--- cliveros.cpp
+++ cliveros.cpp
@@ -5,16 +5,26 @@
 #include <cstdlib>
 
 // AUTHORS: Preston Parsons, Noah Russeau, Ava Curby
-// GITHUB: TheGamingScout2, NoahR1024, transorsmth
+// GITHUB: GamingScout2, NoahR1024, transorsmth
 // CLIVER OS 
 //A Personal Project for the Prof
 // ALPHA v0.102 Testing Environment
 // *** PATCH NOTES ***
 //
-// v0.103-Alpha  - In Development
+// v0.103-Alpha - Added commands HELLO, NEW, OLD, REPLACE, RENAME, CAT, LIST, RUN, STOP,
+//   UNSAVE, SYSTEM, BYE, GOODBYE but implementation is needed
 // v0.102-Alpha - converted original hash map command data structure into
 //                linked list node-pointer style to increase speed and to allow
 //		  for the new command loading architecture style.  
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+#include <cstdlib>
+
+//https://github.com/Gamingscout2/CliverOS
+
+
 using namespace std;
 
 // Struct to represent a command
@@ -32,6 +42,18 @@ void create();
 void edit();
 void save();
 void quit();
+void hello();
+void newFile();
+void oldFile();
+void replace();
+void renameFile();
+void cat();
+void list();
+void run();
+void stop();
+void unsave();
+void system();
+void bye();
 
 // Linked list to store commands
 Command* commandList = nullptr;
@@ -54,10 +76,23 @@ int main() {
     addCommand("edit", edit);
     addCommand("save", save);
     addCommand("quit", quit);
+    addCommand("hello", hello);
+    addCommand("new", newFile);
+    addCommand("old", oldFile);
+    addCommand("replace", replace);
+    addCommand("rename", renameFile);
+    addCommand("cat", cat);
+    addCommand("list", list);
+    addCommand("run", run);
+    addCommand("stop", stop);
+    addCommand("unsave", unsave);
+    addCommand("system", system);
+    addCommand("bye", bye);
+    addCommand("goodbye", bye);
 
     string command;
 
-    cout << "Welcome to CLIVER OS v0.1\n";
+    cout << "Welcome to CLIVER OS v0.103\n";
     cout << "Type 'help' for commands\n";
 
     while (true) {
@@ -86,15 +121,28 @@ int main() {
 // Function definitions
 void help() {
     cout << "Available commands:\n";
-    cout << "  help   - Display available commands\n";
+    cout << "  help    - Display available commands\n";
     cout << "  print   - Print message to the console\n";
-    cout << "  cliver - A Secret!\n";
-    cout << "  create - Create a new text file\n";
-    cout << "  edit   - Edit an existing text file\n";
-    cout << "  save   - Save changes to a text file\n";
-    cout << "  quit   - Exit the operating system\n";
-    cout << "Written by Preston Parsons, Noah Rousseau 2024, ";
-    cout << "Version 0.1 - Development Alpha";
+    cout << "  cliver  - A Secret!\n";
+    cout << "  create  - Create a new text file\n";
+    cout << "  edit    - Edit an existing text file\n";
+    cout << "  save    - Save changes to a text file\n";
+    cout << "  quit    - Exit the operating system\n";
+    cout << "  hello   - Start a new session, enter your user number\n";
+    cout << "  new     - Start a new program\n";
+    cout << "  old     - Retrieve a program from storage\n";
+    cout << "  replace - Save the current program to storage, overwriting older version\n";
+    cout << "  rename  - Rename the current program\n";
+    cout << "  cat     - List the names of your saved programs (short for CATALOG)\n";
+    cout << "  list    - List the current program\n";
+    cout << "  run     - Run the current program\n";
+    cout << "  stop    - Stop the current run of the program (in case of an infinite loop)\n";
+    cout << "  unsave  - Unsave the current program\n";
+    cout << "  system  - Name the system -- limited to either BASIC (default) or ALGOL\n";
+    cout << "  bye     - End the session\n";
+    cout << "Written by Preston Parsons, AE 2024, ";
+    cout << "Version 0.103 - Development Alpha\n";
+    cout << "https://github.com/Gamingscout2/CliverOS";
 }
 
 void print() {
@@ -156,7 +204,7 @@ void edit() {
                     outFile.close();
                     cout << "Changes saved to '" << filename << "'.\n";
                     return;
-                }
+					}
                 outFile << line << endl;
             }
             outFile.close();
@@ -190,4 +238,64 @@ void save() {
 void quit() {
     cout << "Exiting the operating system.\n";
     exit(0);
+}
+
+void hello() {
+    cout << "Start a new session, enter your user number\n";
+    // Implementation for HELLO command goes here
+}
+
+void newFile() {
+    cout << "Start a new program\n";
+    // Implementation for NEW command goes here
+}
+
+void oldFile() {
+    cout << "Retrieve a program from storage\n";
+    // Implementation for OLD command goes here
+}
+
+void replace() {
+    cout << "Save the current program to storage, overwriting older version\n";
+    // Implementation for REPLACE command goes here
+}
+
+void renameFile() {
+    cout << "Rename the current program\n";
+    // Implementation for RENAME command goes here
+}
+
+void cat() {
+    cout << "List the names of your saved programs (short for CATALOG)\n";
+    // Implementation for CAT command goes here
+}
+
+void list() {
+    cout << "List the current program\n";
+    // Implementation for LIST command goes here
+}
+
+void run() {
+    cout << "Run the current program\n";
+    // Implementation for RUN command goes here
+}
+
+void stop() {
+    cout << "Stop the current run of the program (in case of an infinite loop)\n";
+    // Implementation for STOP command goes here
+}
+
+void unsave() {
+    cout << "Unsave the current program\n";
+    // Implementation for UNSAVE command goes here
+}
+
+void system() {
+    cout << "Name the system -- limited to either BASIC (default) or ALGOL\n";
+    // Implementation for SYSTEM command goes here
+}
+
+void bye() {
+    cout << "End the session\n";
+    // Implementation for BYE command goes here
 }
